adca40f63e83ed8970b116de2e91a0b54d08da02
Paul S. Ferrell <pflarr@cfl-ubuntu.lanl.gov>
2009-10-08 09:04:07 -0600

    Various changes to tanks run scripts.

--- tanks/run_tanks.py
+++ tanks/run_tanks.py
@@ -1,10 +1,13 @@
 #! /usr/bin/python
 
-import time
 import optparse
+import shutil
+import time
 from tanks import Pflanzarr
 
 T = 60*5
+MAX_HIST = 30
+HIST_STEP = 100 
 
 parser = optparse.OptionParser('DATA_DIR easy|medium|hard MAX_TURNS')
 opts, args = parser.parse_args()
@@ -19,6 +22,21 @@ while True:
     start = time.time()
     p = Pflanzarr.Pflanzarr(args[0], args[1])
     p.run(turns)
+
+    path = os.path.join(args[0], 'results')
+    files = os.listdir(path)
+    gameNums = []
+    for file in files:
+        try:
+            gameNums.append( int(file) )
+        except:
+            continue
+
+    gameNums.sort(reverse=True)
+    highest = gameNums[0]
+    for num in gameNums:
+        if highest - MAX_HIST > num and not (num % HIST_STEP == 0):
+            shutil.rmtree(os.path.join(path, num))
 
     diff = time.time() - start
     if diff - T > 0:
