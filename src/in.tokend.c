8d4fea620bb87b83d5045c8d98592864c483d842
Neale Pickett <neale@lanl.gov>
2010-09-02 17:10:11 -0600

    Start at in.tokend (not writing files yet)

---
+++ src/in.tokend.c
@@ -0,0 +1,136 @@
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <ctype.h>
+#include "xxtea.h"
+
+#define itokenlen 3
+
+
+char const consonants[] = "bcdfghklmnprstvz";
+char const vowels[]     = "aeiouy";
+
+#define bubblebabble_len(n) (6*(((n)/2)+1))
+
+/** Compute bubble babble for input buffer.
+ *
+ * The generated output will be of length 6*((inlen/2)+1), including the
+ * trailing NULL.
+ *
+ * Test vectors:
+ *     `' (empty string) `xexax'
+ *     `1234567890'      `xesef-disof-gytuf-katof-movif-baxux'
+ *     `Pineapple'       `xigak-nyryk-humil-bosek-sonax'
+ */
+void
+bubblebabble(char *out, char const *in, const size_t inlen)
+{
+  size_t pos  = 0;
+  int    seed = 1;
+  size_t i    = 0;
+
+  out[pos++] = 'x';
+  while (1) {
+    unsigned char c;
+
+    if (i == inlen) {
+      out[pos++] = vowels[seed % 6];
+      out[pos++] = 'x';
+      out[pos++] = vowels[seed / 6];
+      break;
+    }
+
+    c = in[i++];
+    out[pos++] = vowels[(((c >> 6) & 3) + seed) % 6];
+    out[pos++] = consonants[(c >> 2) & 15];
+    out[pos++] = vowels[((c & 3) + (seed / 6)) % 6];
+    if (i == inlen) {
+      break;
+    }
+    seed = ((seed * 5) + (c * 7) + in[i]) % 36;
+
+    c = in[i++];
+    out[pos++] = consonants[(c >> 4) & 15];
+    out[pos++] = '-';
+    out[pos++] = consonants[c & 15];
+  }
+
+  out[pos++] = 'x';
+  out[pos] = '\0';
+}
+
+int
+main(int argc, char *argv[])
+{
+  char     service[50];
+  char     token[80];
+  uint32_t key[4];
+  size_t   tokenlen;
+
+  /* This ought to be unpredictable enough for a contest */
+  srand((int)time(NULL) * (int)getpid());
+
+  /* Read service name */
+  {
+    size_t len;
+    int    i;
+
+    len = fread(service, 1, sizeof(service) - 1, stdin);
+    for (i = 0; (i < len) && isalnum(service[i]); i += 1);
+    service[i] = '\0';
+  }
+
+  /* Read in that service's key */
+  {
+    FILE   *f = fopen(service, "r");
+    size_t  len;
+
+    if (! f) {
+      printf("Unregistered service");
+      return 0;
+    }
+
+    len = fread(&key, sizeof(uint32_t), 4, f);
+    fclose(f);
+
+    if (4 != len) {
+      printf("Key file screwed up");
+      return 0;
+    }
+  }
+
+  /* Create the token */
+  {
+    uint8_t crap[itokenlen];
+    char digest[bubblebabble_len(itokenlen)];
+    int  i;
+
+    /* Digest some random junk */
+    for (i = 0; i < itokenlen; i += 1) {
+      crap[i] = (uint8_t)random();
+    }
+    bubblebabble(digest, crap, itokenlen);
+
+    /* Append digest to service name */
+    tokenlen = (size_t)snprintf(token, sizeof(token),
+                                "%s:%s",
+                                service, digest);
+  }
+
+  /* Encrypt the token */
+  /* Note that now tokenlen is in uint32_ts, not chars! */
+  {
+    tokenlen = (tokenlen + (tokenlen % 4)) / 4;
+
+    tea_encode(key, (uint32_t *)token, tokenlen);
+  }
+
+  /* Send it back */
+  fwrite(token, tokenlen, sizeof(uint32_t), stdout);
+
+  return 0;
+}
---
+++ src/xxtea.h
@@ -0,0 +1,2 @@
+void tea_encode(uint32_t const key[4], uint32_t *buf, size_t buflen);
+void tea_decode(uint32_t const key[4], uint32_t *buf, size_t buflen);
