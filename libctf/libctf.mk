e70f0a0fc46bdacd02023b6706b3a194f3c19c34
Neale Pickett <neale@lanl.gov>
2011-01-24 13:09:51 -0700

    Finalize tokend changes

--- libctf/libctf.mk
+++
@@ -1,9 +0,0 @@
-libctf: libctf/libctf.a
-libctf/libctf.a: libctf/libctf.a(libctf/md5.o)
-libctf/libctf.a: libctf/libctf.a(libctf/arc4.o)
-libctf/libctf.a: libctf/libctf.a(libctf/rand.o)
-libctf/libctf.a: libctf/libctf.a(libctf/token.o)
-
-clean: libctf-clean
-libctf-clean:
-	rm -f libctf/*.o libctf/libctf.a
