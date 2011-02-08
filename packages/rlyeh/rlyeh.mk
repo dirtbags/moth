5a812c5068daa5bfc3e69c0a23c8369625bea804
Neale Pickett <neale@woozle.org>
2011-02-07 22:58:01 -0700

    Clean up rlyeh.mk

--- packages/rlyeh/rlyeh.mk
+++ packages/rlyeh/rlyeh.mk
@@ -15,28 +15,11 @@ $(RLYEH_BUILDDIR)/rlyeh: $(RLYEH_TAR)
 rlyeh-build: rlyeh-source
 	$(MAKE) -C $(RLYEH_BUILDDIR)/rlyeh
 
-# "html" instead of "www" to prevent automatic links
 rlyeh-install: rlyeh-build
 	mkdir -p $(RLYEH_PKGDIR)/bin
-	cp $(RLYEH_BUILDDIR)/crlyeh/forfrlyeh $(RLYEH_PKGDIR)/bin
-	cp $(RLYEH_BUILDDIR)/crlyeh/designer.cgi $(RLYEH_PKGDIR)/bin
-	cp $(RLYEH_BUILDDIR)/crlyeh/rank.awk $(RLYEH_PKGDIR)/bin
-	cp $(RLYEH_BUILDDIR)/crlyeh/winners.awk $(RLYEH_PKGDIR)/bin
-
-	$(call COPYTREE, packages/rlyeh/html, $(RLYEH_PKGDIR)/html)
-	cp packages/mcp/www/ctf.css $(RLYEH_PKGDIR)/html/style.css
-	cp packages/mcp/www/grunge.png $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/nav.html.inc $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/rlyeh.js $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/forf.html $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/intro.html $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/figures.js $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/procs.html $(RLYEH_PKGDIR)/html
-	cp $(RLYEH_BUILDDIR)/crlyeh/designer.js $(RLYEH_PKGDIR)/html
+	cp $(RLYEH_BUILDDIR)/rlyeh/rlyeh $(RLYEH_PKGDIR)/bin
 
 	$(call COPYTREE, packages/rlyeh/service, $(RLYEH_PKGDIR)/service)
-
-	$(call COPYTREE, $(RLYEH_BUILDDIR)/crlyeh/examples, $(RLYEH_PKGDIR)/examples)
 
 rlyeh-clean:
 	rm -rf $(RLYEH_BUILDDIR)
