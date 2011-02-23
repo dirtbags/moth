9d2233c34aa2016c6f9e31ea4cbf9474bcafa322
Neale Pickett <neale@woozle.org>
2011-02-22 18:01:51 -0700

    Add sequence 800

--- packages/sequence/sequence.mk
+++ packages/sequence/sequence.mk
@@ -1,1 +1,9 @@
 $(eval $(call STANDARD_PUZZLE, sequence))
+
+SEQUENCE_SUBMAKES = $(wildcard packages/sequence/*/Makefile)
+SEQUENCE_SUBCLEANS = $(patsubst %/Makefile, %/clean, $(SEQUENCE_SUBMAKES))
+
+sequence-clean: $(SEQUENCE_SUBCLEANS)
+
+packages/sequence/%/clean:
+	$(MAKE) -C $(@D) clean
