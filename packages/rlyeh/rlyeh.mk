a1dd6b8804b45f03c81018b46bf0b7041137fc52
Neale Pickett <neale@lanl.gov>
2011-02-23 17:29:34 -0700

    Fix bugs, add sequence 2000

--- packages/rlyeh/rlyeh.mk
+++ packages/rlyeh/rlyeh.mk
@@ -1,23 +1,17 @@
 RLYEH_PKGDIR = $(TARGET)/rlyeh
 RLYEH_BUILDDIR = $(BUILD)/rlyeh
-RLYEH_TAR = $(CACHE)/rlyeh.tar.gz
-RLYEH_URL = "http://woozle.org/~neale/gitweb.cgi?p=rlyeh;a=snapshot;h=master;sf=tgz"
+RLYEH_URL = "http://woozle.org/~neale/projects/rlyeh"
 
-$(RLYEH_TAR):
-	@ mkdir -p $(@D)
-	wget -O $@ $(RLYEH_URL)
-
-rlyeh-source: $(RLYEH_BUILDDIR)/rlyeh
-$(RLYEH_BUILDDIR)/rlyeh: $(RLYEH_TAR)
-	mkdir -p $(RLYEH_BUILDDIR)
-	zcat $(RLYEH_TAR) | (cd $(RLYEH_BUILDDIR) && tar xf -)
+rlyeh-source: $(RLYEH_BUILDDIR)
+$(RLYEH_BUILDDIR):
+	git clone $(RLYEH_URL) $@
 
 rlyeh-build: rlyeh-source
-	$(MAKE) -C $(RLYEH_BUILDDIR)/rlyeh
+	$(MAKE) -C $(RLYEH_BUILDDIR)
 
 rlyeh-install: rlyeh-build
 	mkdir -p $(RLYEH_PKGDIR)/bin
-	cp $(RLYEH_BUILDDIR)/rlyeh/rlyeh $(RLYEH_PKGDIR)/bin
+	cp $(RLYEH_BUILDDIR)/rlyeh $(RLYEH_PKGDIR)/bin
 
 	$(call COPYTREE, packages/rlyeh/service, $(RLYEH_PKGDIR)/service)
 	$(call COPYTREE, packages/rlyeh/tokens, $(RLYEH_PKGDIR)/tokens)
