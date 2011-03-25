d260a2d55a70f5cc56ec820bd19d9e6ee27a69de
Neale Pickett <neale@lanl.gov>
2011-03-25 16:20:23 -0600

    Move puzzles into their own repo

--- packages/net-re/6100/exe.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("The key is 'intangible'.");
-}
--- packages/sequence/sequence.mk
+++
@@ -1,9 +0,0 @@
-$(eval $(call STANDARD_PUZZLE, sequence))
-
-SEQUENCE_SUBMAKES = $(wildcard packages/sequence/*/Makefile)
-SEQUENCE_SUBCLEANS = $(patsubst %/Makefile, %/clean, $(SEQUENCE_SUBMAKES))
-
-sequence-clean: $(SEQUENCE_SUBCLEANS)
-
-packages/sequence/%/clean:
-	$(MAKE) -C $(@D) clean
