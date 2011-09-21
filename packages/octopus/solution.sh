a82fbfe7ba2bb99535aba7716a82b27edf6ba544
Neale Pickett <neale@lanl.gov>
2011-09-21 16:19:00 -0600

    octopus port to ipv6

--- packages/octopus/solution.sh
+++ packages/octopus/solution.sh
@@ -1,16 +1,16 @@
 #! /bin/sh
 
 port=8888
-host=${1:-10.0.0.8}
+host=${1:-[::1]}
 
 blooper=$(tempfile)
 trap "rm $blooper" 0
 
-echo foo | socat -t 0.01 STDIO UDP:$host:$port | tail -n +5 > $blooper
+echo foo | socat -t 0.01 STDIO UDP6:$host:$port | tail -n +5 > $blooper
 
 for i in $(seq 8); do
-    result=$(socat -t 0.01 STDIO UDP:$host:$port < $blooper | awk -F': ' '(NF > 1) {print $2; exit;}')
-    port=$(echo "ibase=8; $result" | bc)
+    result=$(socat -t 0.01 STDIO UDP6:$host:$port < $blooper | awk -F': ' '(NF > 1) {print $2; exit;}')
+    port=$(printf "%d" "0$result")
     echo "next port: $port ($result)"
 done
 echo $result
