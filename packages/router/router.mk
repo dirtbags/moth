95dd33d1b8c2d78e217f8e37d341d25632764af9
Neale Pickett <neale@lanl.gov>
2011-09-22 18:41:28 -0600

    remove dnsmasq from router package

--- packages/router/router.mk
+++ packages/router/router.mk
@@ -2,40 +2,17 @@ ROUTER_PKGDIR = $(TARGET)/router
 ROUTER_BUILDDIR = $(BUILD)/router
 
 
-router-source: $(ROUTER_BUILDDIR)/dnsmasq-source $(ROUTER_BUILDDIR)/radvd-source
+router-source: $(ROUTER_BUILDDIR)/radvd-source
 
-router-build: $(ROUTER_BUILDDIR)/dnsmasq-build $(ROUTER_BUILDDIR)/radvd-build
+router-build: $(ROUTER_BUILDDIR)/radvd-build
 
 router-install: router-build
 	mkdir -p $(ROUTER_PKGDIR)/bin
-	cp $(DNSMASQ_SRCDIR)/src/dnsmasq $(ROUTER_PKGDIR)/bin/
 
 	cp $(RADVD_SRCDIR)/radvd $(ROUTER_PKGDIR)/bin/
 	cp $(RADVD_SRCDIR)/radvdump $(ROUTER_PKGDIR)/bin/
 
 	$(call COPYTREE, packages/router/service, $(ROUTER_PKGDIR)/service)
-
-##
-## dnsmasq
-##
-DNSMASQ_VERSION = 2.57
-DNSMASQ_SRCDIR = $(ROUTER_BUILDDIR)/dnsmasq-$(DNSMASQ_VERSION)
-DNSMASQ_TARBALL = $(CACHE)/dnsmasq-$(DNSMASQ_VERSION).tar.gz
-DNSMASQ_URL = http://www.thekelleys.org.uk/dnsmasq/dnsmasq-$(DNSMASQ_VERSION).tar.gz
-
-$(DNSMASQ_TARBALL):
-	@ mkdir -p $(@D)
-	wget -O $@ $(DNSMASQ_URL)
-
-$(ROUTER_BUILDDIR)/dnsmasq-source: $(DNSMASQ_TARBALL)
-	mkdir -p $(ROUTER_BUILDDIR)
-	zcat $(DNSMASQ_TARBALL) | (cd $(ROUTER_BUILDDIR) && tar xf -)
-	touch $@
-
-$(ROUTER_BUILDDIR)/dnsmasq-build: $(ROUTER_BUILDDIR)/dnsmasq-source
-	$(MAKE) -C $(DNSMASQ_SRCDIR)
-	touch $@
-
 
 ##
 ## radvd
