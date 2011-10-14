5e725489a1bf89318f9a82794aad9a35446f8f05
Neale Pickett <neale@lanl.gov>
2011-10-13 21:15:11 -0600

    add multicaster, verify token junk

--- packages/multicaster/multicaster.mk
+++ packages/multicaster/multicaster.mk
@@ -2,6 +2,9 @@ MULTICASTER_PKGDIR = $(TARGET)/multicaster
 
 multicaster-install: multicaster-build
 	mkdir -p $(MULTICASTER_PKGDIR)
+	cp packages/multicaster/tokens.txt $(MULTICASTER_PKGDIR)
+
+	$(call COPYTREE, packages/multicaster/service, $(MULTICASTER_PKGDIR)/service)
 
 	mkdir -p $(MULTICASTER_PKGDIR)/bin/
 	$(MAKE) -C packages/multicaster/src install DESTDIR=$(CURDIR)/$(MULTICASTER_PKGDIR)
