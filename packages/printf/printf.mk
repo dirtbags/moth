2874770478137782174ecc5c136fab439e6c22e1
Neale Pickett <neale@lanl.gov>
2011-10-14 22:10:25 -0600

    printf working

--- packages/printf/printf.mk
+++ packages/printf/printf.mk
@@ -7,6 +7,7 @@ printf-install: printf-build
 	$(MAKE) -C packages/printf/src install DESTDIR=$(CURDIR)/$(PRINTF_PKGDIR)
 
 	$(call COPYTREE, packages/printf/service, $(PRINTF_PKGDIR)/service)
+	cp packages/printf/tokens.txt $(PRINTF_PKGDIR)/
 
 printf-clean:
 	rm -rf $(PRINTF_PKGDIR)
