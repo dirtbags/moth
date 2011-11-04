728152af507741b92c27e7c8dabbd25c29c1f34f
Neale Pickett <neale@lanl.gov>
2011-11-03 22:22:19 -0600

    fix revwords build

--- packages/revwords/revwords.mk
+++ packages/revwords/revwords.mk
@@ -7,14 +7,13 @@ revwords-install: revwords-build
 	$(call COPYTREE, packages/revwords/service, $(REVWORDS_PKGDIR)/service)
 
 	cp packages/revwords/tokens.txt $(REVWORDS_PKGDIR)/
-	cp $(REVWORDS_BUILDDIR)/token.enc $(REVWORDS_PKGDIR)/
 	cp packages/revwords/src/revwords $(REVWORDS_PKGDIR)/bin/
 
 revwords-clean:
 	rm -rf $(REVWORDS_PKGDIR) $(REVWORDS_BUILDDIR)
 	$(MAKE) -C packages/revwords/src clean
 
-revwords-build: $(REVWORDS_BUILDDIR)/token.enc
+revwords-build:
 	$(MAKE) -C packages/revwords/src build
 
 PACKAGES += revwords
