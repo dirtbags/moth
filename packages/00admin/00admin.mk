a12cf49a7d7693a0f9f8cd643eb7ca3fbf5a95c2
Neale Pickett <neale@lanl.gov>
2012-04-19 17:40:30 -0600

    reintegrate ipv4, tanks

--- packages/00admin/00admin.mk
+++ packages/00admin/00admin.mk
@@ -2,5 +2,7 @@
 
 00admin-install:
 	$(call COPYTREE, packages/00admin/service, $(00ADMIN_PKGDIR)/service)
+	mkdir -p $(00ADMIN_PKGDIR)/sbin
+	cp packages/00admin/sbin/* $(00ADMIN_PKGDIR)/sbin
 
 PACKAGES += 00admin
