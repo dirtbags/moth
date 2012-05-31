58bc201f03f2b2f5d7fbea141c98ecd3897256b7
Neale Pickett <neale@lanl.gov>
2012-05-30 18:04:24 -0600

    move mcp to new points architecture

--- packages/mcp/src/common.h
+++ packages/mcp/src/common.h
@@ -9,28 +9,29 @@
 #define TOKEN_MAX 80
 #define itokenlen 5
 
+#define ERR_GENERAL -1
+#define ERR_NOTEAM -2
+#define ERR_CLAIMED -3
+
 int cgi_init(char *global_argv[]);
 size_t cgi_item(char *str, size_t maxlen);
 void cgi_head(char *title);
 void cgi_foot();
 void cgi_result(int code, char *desc, char *fmt, ...);
+void cgi_fail(int err);
 void cgi_page(char *title, char *fmt, ...);
 void cgi_error(char *text);
 
 
-int fgrepx(char const *needle, char const *filename);
+int anchored_search(char const *filename, char const *needle, const char anchor);
 void urandom(char *buf, size_t buflen);
 int my_snprintf(char *buf, size_t buflen, char *fmt, ...);
 char *state_path(char const *fmt, ...);
 char *package_path(char const *fmt, ...);
 int team_exists(char const *teamhash);
-int award_points(char const *teamhacsh,
+int award_points(char const *teamhash,
                  char const *category,
-                 long point);
-void award_and_log_uniquely(char const *team,
-                            char const *category,
-                            long points,
-                            char const *logfile,
-                            char const *line);
+                 long point,
+                 char const *uid);
 
 #endif
