16c9dc94e565ca403cf8de9e4c955e34c3d9beba
Neale Pickett <neale@woozle.org>
2012-06-09 16:11:23 -0600

    Start at p2 server package

--- packages/00admin/00admin.mk
+++ packages/00admin/00admin.mk
@@ -1,13 +1,20 @@
 00ADMIN_PKGDIR = $(TARGET)/00admin
+00ADMIN_BUILDDIR = $(BUILD)/00admin
 
 ifndef PASSWORD
 $(error PASSWORD not defined)
 endif
 
-00admin-install:
+00admin-build: $(00ADMIN_BUILDDIR)/build
+$(00ADMIN_BUILDDIR)/build:
+	$(MAKE) -C packages/00admin/src
+	touch $<
+	
+00admin-install: $(00ADMIN_BUILDDIR)/build
 	$(call COPYTREE, packages/00admin/service, $(00ADMIN_PKGDIR)/service)
 	echo "$(PASSWORD)" > $(00ADMIN_PKGDIR)/password
 	mkdir -p $(00ADMIN_PKGDIR)/sbin
-	cp packages/00admin/sbin/* $(00ADMIN_PKGDIR)/sbin
+	cp packages/00admin/bin/* $(00ADMIN_PKGDIR)/bin
+	cp packages/00admin/src/tea $(00ADMIN_PKGDIR)/bin
 
 PACKAGES += 00admin
