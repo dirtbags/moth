3d32295d0b736be1b0920c06e9e27ae39715344d
Neale Pickett <neale@woozle.org>
2012-06-09 15:50:45 -0600

    s/sbin/bin/ for fire-ip

--- packages/inferno/inferno.mk
+++ packages/inferno/inferno.mk
@@ -1,24 +1,14 @@
 INFERNO_PKGDIR = $(TARGET)/inferno
 INFERNO_BUILDDIR = $(BUILD)/inferno
 
-INFERNO_ERIS_CACHE = $(CACHE)/eris.git
-INFERNO_ERIS_URL = http://woozle.org/~neale/projects/eris
+inferno-source:
 
-$(INFERNO_ERIS_CACHE):
-	git clone --bare $(INFERNO_ERIS_URL) $@
+inferno-build: 
 
-inferno-source: $(INFERNO_BUILDDIR)
-$(INFERNO_BUILDDIR): $(INFERNO_ERIS_CACHE)
-	git clone $< $@
-
-inferno-build: $(INFERNO_BUILDDIR)/build
-$(INFERNO_BUILDDIR)/build: $(INFERNO_BUILDDIR)
-	$(MAKE) -C $(INFERNO_BUILDDIR)
-
-inferno-install: $(INFERNO_BUILDDIR)/build
+inferno-install: eris
 	mkdir -p $(INFERNO_PKGDIR)/bin
 
-	cp $(INFERNO_BUILDDIR)/eris $(INFERNO_PKGDIR)/bin/
+	cp $(ERIS_BIN) $(INFERNO_PKGDIR)/bin/
 
 	$(call COPYTREE, packages/inferno/service, $(INFERNO_PKGDIR)/service)
 
