279920e1452df360c91dce3cd9f8336faf698346
Neale Pickett <neale@woozle.org>
2012-06-09 15:50:45 -0600

    s/sbin/bin/ for fire-ip

--- packages/mcp/mcp.mk
+++ packages/mcp/mcp.mk
@@ -1,31 +1,16 @@
 MCP_PKGDIR = $(TARGET)/mcp
 MCP_BUILDDIR = $(BUILD)/mcp
 
-
-##
-## XXX: clean up eris like router.mk
-##
-
-ERIS_CACHE = $(CACHE)/eris.git
-ERIS_BUILDDIR = $(MCP_BUILDDIR)/eris
-ERIS_URL = http://woozle.org/~neale/projects/eris
-
-$(ERIS_CACHE):
-	git clone --bare $(ERIS_URL) $@
-
 mcp-source: $(MCP_BUILDDIR)/source
-$(MCP_BUILDDIR)/source: $(ERIS_CACHE)
+$(MCP_BUILDDIR)/source:
 	mkdir -p $(@D)
-	git clone $(ERIS_CACHE) $(@D)/eris
 	touch $@
 
 mcp-build: $(MCP_BUILDDIR)/build
 $(MCP_BUILDDIR)/build: $(MCP_BUILDDIR)/source
 	$(MAKE) -C packages/mcp/src build
-	$(MAKE) -C $(MCP_BUILDDIR)/eris
 
-
-mcp-install: $(MCP_BUILDDIR)/build
+mcp-install: $(MCP_BUILDDIR)/build eris
 	mkdir -p $(MCP_PKGDIR)
 
 	$(call COPYTREE, packages/mcp/bin, $(MCP_PKGDIR)/bin)
@@ -33,7 +18,7 @@ mcp-install: $(MCP_BUILDDIR)/build
 	cp packages/mcp/src/puzzles.cgi $(MCP_PKGDIR)/bin/
 	cp packages/mcp/src/tea $(MCP_PKGDIR)/bin/
 
-	cp $(MCP_BUILDDIR)/eris/eris $(MCP_PKGDIR)/bin/
+	cp $(ERIS_BIN) $(MCP_PKGDIR)/bin/
 
 	$(call COPYTREE, packages/mcp/service, $(MCP_PKGDIR)/service)
 
