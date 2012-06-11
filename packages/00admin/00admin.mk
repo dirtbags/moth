81d52e3bdfd9d668e4b51f0cd13ca2cc58dcfbc7
Neale Pickett <neale@lanl.gov>
2012-06-11 14:37:39 -0600

    Working, tested p2 console

--- packages/00admin/00admin.mk
+++ packages/00admin/00admin.mk
@@ -8,7 +8,6 @@ endif
 00admin-build: $(00ADMIN_BUILDDIR)/build
 $(00ADMIN_BUILDDIR)/build:
 	$(MAKE) -C packages/00admin/src
-	touch $<
 	
 00admin-install: $(00ADMIN_BUILDDIR)/build
 	$(call COPYTREE, packages/00admin/service, $(00ADMIN_PKGDIR)/service)
