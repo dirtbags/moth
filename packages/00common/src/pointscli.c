86cc9c3524413cb04c0e667627dadbbecd9e6736
Neale Pickett <neale@lanl.gov>
2012-06-13 11:11:26 -0600

    pointscli no longer fails on success

--- packages/00common/src/pointscli.c
+++ packages/00common/src/pointscli.c
@@ -7,38 +7,41 @@
 int
 main(int argc, char *argv[])
 {
-  int points;
-  int ret;
-  char comment[512];
+    int             points;
+    int             ret;
+    char            comment[512];
 
-  if (argc != 5) {
-    fprintf(stderr, "Usage: pointscli TEAM CATEGORY POINTS 'COMMENT'\n");
-    return EX_USAGE;
-  }
+    if (argc != 5) {
+        fprintf(stderr,
+                "Usage: pointscli TEAM CATEGORY POINTS 'COMMENT'\n");
+        return EX_USAGE;
+    }
 
-  points = atoi(argv[3]);
-  if (0 == points) {
-    fprintf(stderr, "Error: award 0 points?\n");
-    return EX_USAGE;
-  }
+    points = atoi(argv[3]);
+    if (0 == points) {
+        fprintf(stderr, "Error: award 0 points?\n");
+        return EX_USAGE;
+    }
 
-  snprintf(comment, sizeof comment, "--%s", argv[4]);
+    snprintf(comment, sizeof comment, "--%s", argv[4]);
 
-  ret = award_points(argv[1], argv[2], points, comment);
-  switch (ret) {
-    case ERR_GENERAL:
-      perror("General error");
-      return EX_UNAVAILABLE;
-    case ERR_NOTEAM:
-      fprintf(stderr, "No such team\n");
-      return EX_NOUSER;
-    case ERR_CLAIMED:
-      fprintf(stderr, "Duplicate entry\n");
-      return EX_DATAERR;
-    default:
-      fprintf(stderr, "Error %d\n", ret);
-      return EX_SOFTWARE;
-  }
+    ret = award_points(argv[1], argv[2], points, comment);
+    switch (ret) {
+        case 0:
+            return 0;
+        case ERR_GENERAL:
+            perror("General error");
+            return EX_UNAVAILABLE;
+        case ERR_NOTEAM:
+            fprintf(stderr, "No such team\n");
+            return EX_NOUSER;
+        case ERR_CLAIMED:
+            fprintf(stderr, "Duplicate entry\n");
+            return EX_DATAERR;
+        default:
+            fprintf(stderr, "Error %d\n", ret);
+            return EX_SOFTWARE;
+    }
 
-  return 0;
+    return 0;
 }
