f99c87b01e80e12453da1f4cf118770ac3bd6d59
Neale Pickett <neale@lanl.gov>
2012-07-12 17:15:43 -0600

    lots of little fixups, caught up with todo after trail run in ACS

--- packages/p2client/p2client.mk
+++ packages/p2client/p2client.mk
@@ -7,6 +7,7 @@ p2client-install:
 
 	$(call COPYTREE, packages/p2client/service, $(P2CLIENT_PKGDIR)/service)
 	loadkeys -b packages/p2client/dumbterm.map > $(P2CLIENT_PKGDIR)/dumbterm.kmap
+	cp packages/p2client/lite-16.fnt $(P2CLIENT_PKGDIR)
 
 p2client-clean:
 	rm -rf $(P2CLIENT_PKGDIR)
