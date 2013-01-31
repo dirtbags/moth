0a6f84f483f68026bab906bb2da21d5c7cbefc45
Neale Pickett <neale@lanl.gov>
2013-01-31 13:51:56 -0700

    add tokens for netre

--- doc/2013-02-TF5/spam.py
+++ doc/2013-02-TF5/spam.py
@@ -1,14 +1,27 @@
 #! /usr/bin/python3
 
 import smtplib
+import sys
+
+courses = {
+	"net": "Network Archaeology",
+	"mal": "Malware Reverse-Engineering",
+	"hst": "Host Forensics",
+	"icc": "Incident Coordination",
+	"nil": "None",
+}
 
 smtpd = smtplib.SMTP("mail.lanl.gov")
 
-template = open("hubs-mail.txt").read()
-
-for line in open("assignments.txt"):
-	course, email = line.strip().split()
-
-	print(email)
-	msg = template.replace("RCPT", email)
-	smtpd.sendmail("neale@lanl.gov", [email], msg)
+template = sys.stdin.read()
+if 'RCPT' not in template:
+	print("Pass the template on stdin.")
+else:
+	for line in open("assignments.txt"):
+		course, email = line.strip().split()
+		coursename = courses[course]
+	
+		print(email)
+		msg = template.replace("RCPT", email).replace("COURSE", coursename)
+		smtpd.sendmail("neale@lanl.gov", [email], msg)
+		#print(msg)
