20983f810fa9e7febb79e53228bb098960e0c704
Neale Pickett <neale@woozle.org>
2013-02-06 00:32:41 -0700

    oops file name II

--- packages/host/host.mk
+++ packages/host/host.mk
@@ -2,7 +2,7 @@ HOST_PKGDIR = $(TARGET)/host
 
 host-install:
 	mkdir -p $(HOST_PKGDIR)
-	cp packages/host/answers.txt $(HOST_PKGDIR)
+	cp packages/host/tokens.txt $(HOST_PKGDIR)
 
 PACKAGES += host
 
