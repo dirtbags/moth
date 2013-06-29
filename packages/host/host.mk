54f793a43273647d68d4bcdd90443ba3ba2faf71
Neale Pickett <neale@woozle.org>
2013-06-28 19:09:03 -0600

    Remove old things

--- packages/host/host.mk
+++
@@ -1,8 +0,0 @@
-HOST_PKGDIR = $(TARGET)/host
-
-host-install:
-	mkdir -p $(HOST_PKGDIR)
-	cp packages/host/tokens.txt $(HOST_PKGDIR)
-
-PACKAGES += host
-
--- packages/posters/posters.mk
+++
@@ -1,8 +0,0 @@
-posters-source:
-posters-build:
-
-posters-install: packages/posters/tokens.txt
-	mkdir -p $(TARGET)/posters/
-	cp $< $(TARGET)/posters/
-
-PACKAGES += posters
