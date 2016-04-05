06cf79d2ef1f8c2602d3744f92548ab26c1c3bb2
Neale Pickett <neale@woozle.org>
2016-04-05 04:16:22 +0000

    Less prototypey

--- www/res/main.js
+++ www/res/main.js
@@ -3,7 +3,7 @@ var main_terminal;
 function Main(element) {
     var term = new Terminal(element);
 
-    this.start = function() {
+    function start() {
 	term.clear();
 	term.par("Main terminal.")
 	term.par("This is the main terminal. In this terminal you will get your puzzle content and someplace to enter in possible answers. It's probably just going to pull the old URL, steal the body element, and submit it to a new Terminal method for slow-despooling of the content of text nodes.")
@@ -11,12 +11,16 @@ function Main(element) {
 	term.par("Anyway.");
 	term.par("Hopefully this demo illustrates how things are going to look.");
     }
+
+    term.clear();
+    term.par("Loading…");
+
+    setTimeout(start, 2500);
 }
 
 
 function main_start() {
     main_terminal = new Main(document.getElementById("main"));
-    setTimeout(main_terminal.start, 2500);
 }
 
 window.addEventListener("load", main_start);
