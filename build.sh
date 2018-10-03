f3421d7a93ec51df2372bce67f4ff5d76a52871d
Neale Pickett <neale@lanl.gov>
2018-10-03 01:26:40 +0000

    No, I meant push latest

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)
 for img in moth moth-devel; do
     echo "==== $img"
     sudo docker build --build-arg http_proxy=$http_proxy --build-arg https_proxy=$https_proxy --tag dirtbags/$img --tag dirtbags/$img:$version -f Dockerfile.$img .
-    [ "$1" = "-push" ] && docker push dirtbags/$img:$version && docker push dirtbags/$img:$version
+    [ "$1" = "-push" ] && docker push dirtbags/$img:$version && docker push dirtbags/$img:latest
 done
 
 exit 0
