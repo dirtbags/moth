a3636c0a5566d815083431f830fa456331d87696
Neale Pickett <neale@woozle.org>
2019-04-19 16:18:08 +0000

    Back out a change I disagree with

--- devel/mothballer.py
+++ devel/mothballer.py
@@ -59,9 +59,6 @@ def build_category(categorydir, outdir):
     mothball = package(categoryname, categorydir, category_seed)
     shutil.copyfileobj(mothball, zipfileraw)
     zipfileraw.close()
-    zipfiledir = os.path.dirname(zipfilename)
-    if not os.path.exists(zipfiledir):
-        os.makedirs(zipfiledir)
     shutil.move(zipfileraw.name, zipfilename)
 
 
