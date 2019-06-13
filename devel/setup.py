ed6de7a4f1e38fa6b210bf868451ad41776c4864
John Donaldson <donaldson8@llnl.gov>
2019-06-13 23:57:58 +0100

    Adding minimum Python version specifier

--- devel/setup.py
+++ devel/setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup
 
+import sys
+if sys.version_info < (3,5):
+    sys.exit("Sorry, Python < 3.5 is not supported")
+
 setup(
     name = "MOTHDevel",
     version = open("../VERSION", "r").read(),
     description = "The MOTH development toolkit",
     packages = ["MOTHDevel"],
+    python_requires='~=3.5',
     include_package_data=True,
     entry_points = {
         "console_scripts": [
