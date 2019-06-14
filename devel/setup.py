d3a7efc7f1a78e63546b60cee07058ec71481dc5
John Donaldson <donaldson8@llnl.gov>
2019-06-14 20:31:21 +0100

    Changed ordering of test dependencies Added frosted

--- devel/setup.py
+++ devel/setup.py
@@ -10,7 +10,13 @@ setup(
     description = "The MOTH development toolkit",
     packages = ["MOTHDevel"],
     python_requires='~=3.5',
-    tests_require=["flake8==3.7.7", "nose>=1.3.7", "pylint==2.3.1", "coverage==4.5.3"],
+    tests_require=[
+        "coverage==4.5.3", 
+        "flake8==3.7.7", 
+        "frosted==1.4.1",
+        "nose>=1.3.7", 
+        "pylint==2.3.1", 
+    ],
     extras_require={
         "scapy": ["scapy>=2.4.2"],
     },
