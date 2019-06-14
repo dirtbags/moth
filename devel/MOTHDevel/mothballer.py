10696a03cf44a42ddbc5740360bd277f0618c31a
John Donaldson <donaldson8@llnl.gov>
2019-06-14 20:29:29 +0100

    Fixing some linting and PEP8 issues

--- devel/MOTHDevel/mothballer.py
+++ devel/MOTHDevel/mothballer.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python3
+"""Module responsible for compiling Moth puzzles into mothballs"""
 
 import argparse
-import binascii
 import hashlib
 import io
 import json
 import logging
-from . import moth
 import os
 import shutil
 import tempfile
 import zipfile
 import random
 
+from . import moth
+
 SEEDFN = "SEED"
 
 
-def write_kv_pairs(ziphandle, filename, kv):
+def write_kv_pairs(ziphandle, filename, data):
     """ Write out a sorted map to file
     :param ziphandle: a zipfile object
     :param filename: The filename to write within the zipfile object
@@ -24,83 +25,98 @@ def write_kv_pairs(ziphandle, filename, kv):
     :return:
     """
     filehandle = io.StringIO()
-    for key in sorted(kv.keys()):
-        if isinstance(kv[key], list):
-            for val in kv[key]:
+    for key in sorted(data.keys()):
+        if isinstance(data[key], list):
+            for val in data[key]:
                 filehandle.write("%s %s\n" % (key, val))
         else:
-            filehandle.write("%s %s\n" % (key, kv[key]))
+            filehandle.write("%s %s\n" % (key, data[key]))
     filehandle.seek(0)
     ziphandle.writestr(filename, filehandle.read())
 
 
-def escape(s):
-    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+def escape(instr):
+    """Escape common HTML sequences
+    :param instr: string to escape
+    :return: the escaped string
+    """
+    return instr.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
 
 
 def build_category(categorydir, outdir):
+    """Build a category and save it as a mothball
+    :param categorydir: the path to the directory containing category Moth files
+    :param outdir: the path to the directory to which generated mothballs will be written
+    :return:
+    """
     category_seed = random.getrandbits(32)
 
     categoryname = os.path.basename(categorydir.strip(os.sep))
     zipfilename = os.path.join(outdir, "%s.mb" % categoryname)
-    logging.info("Building {} from {}".format(zipfilename, categorydir))
+    logging.info("Building %s from %s", zipfilename, categorydir)
 
     if os.path.exists(zipfilename):
         # open and gather some state
         existing = zipfile.ZipFile(zipfilename, 'r')
         try:
             category_seed = int(existing.open(SEEDFN).read().strip())
-        except Exception:
+        except (ValueError, KeyError):  # If the seed is malformed or doesn't exist
             pass
         existing.close()
-    logging.debug("Using PRNG seed {}".format(category_seed))
+    logging.debug("Using PRNG seed %s", category_seed)
 
     zipfileraw = tempfile.NamedTemporaryFile(delete=False)
-    mothball = package(categoryname, categorydir, category_seed)
+    mothball = package(categorydir, category_seed)
     shutil.copyfileobj(mothball, zipfileraw)
     zipfileraw.close()
     shutil.move(zipfileraw.name, zipfilename)
 
 
-# Returns a file-like object containing the contents of the new zip file
-def package(categoryname, categorydir, seed):
+def package(categorydir, seed):
+    """Return a file-like object containing the contents of the new zip file
+    :param categorydir: The path to the directory containing category Moth files
+    :param seed: A seed value used to initialize the PSRNG used by some puzzles
+    :return: A file-like object containing the contents of the new zip file
+    """
+    # pylint: disable=too-many-locals
     zfraw = io.BytesIO()
-    zf = zipfile.ZipFile(zfraw, 'x')
-    zf.writestr("category_seed.txt", str(seed))
+    with zipfile.ZipFile(zfraw, 'x') as mothball:
+        mothball.writestr("category_seed.txt", str(seed))
 
-    cat = moth.Category(categorydir, seed)
-    mapping = {}
-    answers = {}
-    summary = {}
-    for puzzle in cat:
-        logging.info("Processing point value {}".format(puzzle.points))
+        cat = moth.Category(categorydir, seed)
+        mapping = {}
+        answers = {}
+        summary = {}
+        for puzzle in cat:
+            logging.info("Processing point value %s", puzzle.points)
 
-        hashmap = hashlib.sha1(str(seed).encode('utf-8'))
-        hashmap.update(str(puzzle.points).encode('utf-8'))
-        puzzlehash = hashmap.hexdigest()
+            hashmap = hashlib.sha1(str(seed).encode('utf-8'))
+            hashmap.update(str(puzzle.points).encode('utf-8'))
+            puzzlehash = hashmap.hexdigest()
 
-        mapping[puzzle.points] = puzzlehash
-        answers[puzzle.points] = puzzle.answers
-        summary[puzzle.points] = puzzle.summary
+            mapping[puzzle.points] = puzzlehash
+            answers[puzzle.points] = puzzle.answers
+            summary[puzzle.points] = puzzle.summary
 
-        puzzledir = os.path.join("content", puzzlehash)
-        for fn, f in puzzle.files.items():
-            payload = f.stream.read()
-            zf.writestr(os.path.join(puzzledir, fn), payload)
+            puzzledir = os.path.join("content", puzzlehash)
+            for filename, file_obj in puzzle.files.items():
+                payload = file_obj.stream.read()
+                mothball.writestr(os.path.join(puzzledir, filename), payload)
 
-        obj = puzzle.package()
-        zf.writestr(os.path.join(puzzledir, 'puzzle.json'), json.dumps(obj))
+            obj = puzzle.package()
+            mothball.writestr(os.path.join(puzzledir, 'puzzle.json'), json.dumps(obj))
 
-    write_kv_pairs(zf, 'map.txt', mapping)
-    write_kv_pairs(zf, 'answers.txt', answers)
-    write_kv_pairs(zf, 'summaries.txt', summary)
+        write_kv_pairs(mothball, 'map.txt', mapping)
+        write_kv_pairs(mothball, 'answers.txt', answers)
+        write_kv_pairs(mothball, 'summaries.txt', summary)
 
     # clean up
-    zf.close()
     zfraw.seek(0)
     return zfraw
 
+
 def main(args=None):
+    """Main interface"""
     if args is None:
         import sys  # pragma: nocover
         args = sys.argv  # pragma: nocover
@@ -116,6 +132,7 @@ def main(args=None):
     for categorydir in args.categorydirs:
         categorydir = os.path.abspath(categorydir)
         build_category(categorydir, outdir)
+
 
 if __name__ == '__main__':
     main()  # pragma: nocover
