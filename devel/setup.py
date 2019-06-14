47341a172e17a94046376a01e315484a26a28a78
John Donaldson <donaldson8@llnl.gov>
2019-06-14 20:02:02 +0100

    Fixing some setup issues

--- devel/setup.py
+++ devel/setup.py
@@ -6,10 +6,14 @@ if sys.version_info < (3,5):
 
 setup(
     name = "MOTHDevel",
-    version = open("../VERSION", "r").read(),
+    version = open("../VERSION", "r").read().strip(),
     description = "The MOTH development toolkit",
     packages = ["MOTHDevel"],
     python_requires='~=3.5',
+    tests_require=["flake8==3.7.7", "nose>=1.3.7", "pylint==2.3.1", "coverage==4.5.3"],
+    extras_require={
+        "scapy": ["scapy>=2.4.2"],
+    },
     include_package_data=True,
     entry_points = {
         "console_scripts": [
