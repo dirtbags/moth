b5b578e08ab2c7ab68a8b2b5bdf0ea5eabbcf90d
Neale Pickett <neale@lanl.gov>
2019-11-04 16:22:44 +0000

    We use ASI

--- theme/moth-pwa.js
+++ theme/moth-pwa.js
@@ -2,15 +2,15 @@ function pwa_init() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register("./sw.js").then(function(reg) {
     }).catch(function(err) {
-      console.warn("Error while registering service worker", err);
-    });
+      console.warn("Error while registering service worker", err)
+    })
   } else {
-    console.log("Service workers not supported. Some offline functionality may not work");
+    console.log("Service workers not supported. Some offline functionality may not work")
   }
 }
 
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", pwa_init);
+  document.addEventListener("DOMContentLoaded", pwa_init)
 } else {
-  pwa_init();
+  pwa_init()
 }
--- theme/sw.js
+++ theme/sw.js
@@ -8,42 +8,42 @@ var content = [
   "moth.js",
   "sw.js",
   "points.json",
-];
+]
 
 self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(content).then(
         function() {
-          self.skipWaiting();
-        });
+          self.skipWaiting()
+        })
     })
-  );
-});
+  )
+})
 
 /* Attempt to fetch live resources, first, then fall back to cache */
 self.addEventListener('fetch', function(event) {
-  let cache_used = false;
+  let cache_used = false
 
   event.respondWith(
     fetch(event.request)
      .catch(function(evt) {
-      //console.log("Falling back to cache for " + event.request.url);
-      cache_used = true;
-      return caches.match(event.request, {ignoreSearch: true});
+      //console.log("Falling back to cache for " + event.request.url)
+      cache_used = true
+      return caches.match(event.request, {ignoreSearch: true})
     }).then(function(res) {
       if (res && res.ok) {
-        let res_clone = res.clone();
+        let res_clone = res.clone()
 	if (! cache_used && event.request.method == "GET" ) {
           caches.open(cacheName).then(function(cache) {
-            cache.put(event.request, res_clone);
-            //console.log("Storing " + event.request.url + " in cache");
-          });
+            cache.put(event.request, res_clone)
+            //console.log("Storing " + event.request.url + " in cache")
+          })
 	} 
-        return res;
+        return res
       } else {
-        console.log("Failed to retrieve resource");
+        console.log("Failed to retrieve resource")
       }
     })
-  );
-});
+  )
+})
