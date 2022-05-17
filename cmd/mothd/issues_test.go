368e7306c31bdae61236508052fd0545c0437481
John Donaldson <donaldson8@llnl.gov>
2022-05-17 14:08:40 -0700

    Fixing issue with TestIssue156 due to change in registration behavior

--- cmd/mothd/issues_test.go
+++ cmd/mothd/issues_test.go
@@ -13,16 +13,19 @@ func TestIssue156(t *testing.T) {
 	server := NewMothServer(Configuration{}, theme, state, puzzles)
 
 	afero.WriteFile(state, "teams/bloop", []byte("bloop: the team"), 0644)
+	afero.WriteFile(state, "teamids.txt", []byte("bloop"), 0644)
 	state.refresh()
 
 	handler := server.NewHandler("", "bloop")
 	es := handler.ExportState()
 	if _, ok := es.TeamNames["self"]; !ok {
+		t.Log("Team 'bloop' not found in exported state")
 		t.Fail()
 	}
 
 	err := handler.Register("bloop: the other team")
 	if err != ErrAlreadyRegistered {
+		t.Logf("Expected ErrAlreadyRegistered when registering team, received %s, instead", err)
 		t.Fail()
 	}
 }
