a896788cc5f979fba0b45d3dd4602c787c598cbd
Neale Pickett <neale.pickett@pnnl.gov>
2023-09-08 11:31:41 -0600

    Also list KSAs by Category

--- theme/reports/ksa.mjs
+++ theme/reports/ksa.mjs
@@ -24,6 +24,20 @@ async function init() {
     let puzzles = state.Puzzles()
     for (let puzzle of puzzles) {
         await puzzle.Populate().catch(x => {})
+    }
+
+    doing("Filling tables")
+    let KSAsByCategory = {}
+    for (let puzzle of puzzles) {
+        let KSAs = KSAsByCategory[puzzle.Category]
+        if (!KSAs) {
+            KSAs = new Set()
+            KSAsByCategory[puzzle.Category] = KSAs
+        }
+        for (let KSA of (puzzle.KSAs || [])) {
+            KSAs.add(KSA)
+        }
+
         for (let tbody of document.querySelectorAll("tbody")) {
             let row = puzzlerowTemplate.content.cloneNode(true)
             row.querySelector(".category").textContent = puzzle.Category
@@ -31,6 +45,20 @@ async function init() {
             row.querySelector(".ksas").textContent = (puzzle.KSAs || []).join(" ")
             row.querySelector(".error").textContent = puzzle.Error.Body
             tbody.appendChild(row)
+        }
+    }
+
+    doing("Filling KSAs By Category")
+    for (let div of document.querySelectorAll(".KSAsByCategory")) {
+        for (let category of state.Categories()) {
+            let KSAs = [...KSAsByCategory[category]]
+            KSAs.sort()
+
+            div.appendChild(document.createElement("h3")).textContent = category
+            let ul = div.appendChild(document.createElement("ul"))
+            for (let k of KSAs) {
+                ul.appendChild(document.createElement("li")).textContent = k
+            }
         }
     }
 
